7de5ad76235504a45a4c5c646ce81b5bba801b1f
light <light0x00@163.com>
2019-08-24 12:26:35 +0800

    ci

--- deploy/deploy-screen.sh
+++ deploy/deploy-screen.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 echo "after deploy!!!!"
+
+ls $deployPath/$jar_name
+
+#jar_name='blog-api.war'
+#screen -S $appName -X quit
+#screen -S $appName java -jar $deployPath/$jar_name
--- deploy/travis.sh
+++ deploy/travis.sh
@@ -6,11 +6,13 @@ $base_path/deploy/ci-shell/src/index.sh \
 --skip-compile \
 --app-name=blog \
 --local-path=$base_path \
---compile-output-path=dist \
+--compile-output-path=blog-web/target/ \
 --remote-ip=47.244.152.125 \
 --remote-user=light \
 --remote-path=/home/light/app/blog \
 --ssh-key=$base_path/id_rsa_light \
 -y \
 --after-deploy=$base_path/deploy/deploy-screen.sh
+
+
 
