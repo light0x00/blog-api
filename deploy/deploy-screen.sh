17eb2a83a9538da7124129909001366ada8055ef
light <light0x00@163.com>
2019-08-25 10:40:09 +0800

    ci

--- deploy/deploy-screen.sh
+++ deploy/deploy-screen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 jar_name='blog-api.jar'
-ls $deploy_path/$jar_name
+ls $remote_path/$jar_name
 
-jar_path=$deploy_path/$jar_name
+jar_path=$remote_path/$jar_name
 
 if ! [ -r $jar_path ] ;then
-    echo 'jar包不存在或没有权限:$jar_path'
+    echo "jar包不存在或没有权限:$jar_path"
     exit 1;
 fi
 
