300643a0c8b907ba812420da08b7850f0c5946a7
light <light0x00@163.com>
2019-08-25 15:35:49 +0800

    ci

--- deploy/index.sh
+++ deploy/index.sh
@@ -1,45 +1,34 @@
 #!/bin/bash
 
-
 if [ -z $TRAVIS_BUILD_DIR ] ;then
-    base_path=/Users/light/Desktop/my-workbench/java/projects/blog-api # 没有通用的办法
+    app_base_path=/Users/light/Desktop/my-workbench/java/projects/blog-api # 没有通用的办法
 else
-    base_path=$TRAVIS_BUILD_DIR
+    app_base_path=$TRAVIS_BUILD_DIR
 fi
 
+ci_base_path=$app_base_path/deploy/ci-shell/
+
 name=blog-api
-repo=$base_path
-assets=blog-web/target/blog-api.jar
-to=remote/home/light/app/blog-api
+repo=$app_base_path
+assets=$app_base_path/blog-web/target/blog-api.jar
+to=/home/light/app/blog-api
 to_ip=47.244.152.125
 to_user=light
-to_key=$base_path/id_rsa_light
+to_key='/Users/light/.ssh/id_rsa_light'
+run_script=app_base_path/deploy/run.sh
 
-
-$base_path/deploy/ci-shell/src/index.sh \
+$ci_base_path/src/index.sh \
 --mode=remote \
 --skip-pull \
 --skip-compile \
 --app-name=$name \
 --local-path=$repo \
---compile-output-path=$asstes \
+--compile-output-path=$assets \
 --remote-ip=$to_ip \
 --remote-user=$to_user \
 --remote-path=$to \
 --ssh-key=$to_key \
+--after-deploy=$run_script \
 -y
 
 
-if [ $? == 0 ] ; then
-    ls $remote_path/$jar_name
-
-    jar_path=$remote_path/$jar_name
-
-    if ! [ -r $jar_path ] ;then
-        echo "jar包不存在或没有权限:$jar_path"
-        exit 1;
-    fi
-    screen -S $app_name -X quit
-    screen -S $app_name java -jar $deployPath/$jar_name
-
-fi
