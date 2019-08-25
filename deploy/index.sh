b2c66742bf0cc3240e617c945c7105d6993cfefc
light <light0x00@163.com>
2019-08-25 15:45:52 +0800

    ci

--- deploy/index.sh
+++ deploy/index.sh
@@ -8,13 +8,20 @@ fi
 
 ci_base_path=$app_base_path/deploy/ci-shell/
 
+# 项目名
 name=blog-api
+# 项目根路径
 repo=$app_base_path
+# 被部署目标路径
 assets=$app_base_path/blog-web/target/blog-api.jar
+# 部署路径
 to=/home/light/app/blog-api
+# 登录
 to_ip=47.244.152.125
 to_user=light
-to_key='/Users/light/.ssh/id_rsa_light'
+# 私钥路径
+to_key="$app_base_path/id_rsa_light"
+# 部署后运行的脚本
 run_script=app_base_path/deploy/run.sh
 
 $ci_base_path/src/index.sh \
