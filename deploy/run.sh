5459cdf24cf4968d6c090ad858429809262b20ec
light <light0x00@163.com>
2019-08-26 11:52:38 +0800

    ci

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,3 +1,3 @@
-# 此脚本的STDUT将会作为部署后,运行应用的脚本.
-echo "screen -S $app_name -X quit ;"
-echo "screen -dmS $app_name java -jar $remote_path/blog-api.jar --MYSQL_BLOG_PWD=\$MYSQL_BLOG_PWD"
+#!/bin/bash
+screen -S blog-api -X quit 2> /dev/null;
+screen -dmS blog-api java -jar /home/light/app/blog-api/blog-api.jar --MYSQL_BLOG_PWD=$MYSQL_BLOG_PWD
