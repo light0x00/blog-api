e1c9b50e52309cd11389f9456c8a451065026714
light <light0x00@163.com>
2019-10-17 17:56:07 +0800

    db structure backup

--- backup/blog.sql
+++ backup/blog.sql
@@ -1,16 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: blog
+-- ------------------------------------------------------
+-- Server version	8.0.16
 
-create database blog;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-use blog;
+--
+-- Table structure for table `msg_comment`
+--
 
-create table msg_comment(
-  id int primary key auto_increment,
-  article_key varchar(100),
-  nickname varchar(30),
-  email varchar(50),
-  post_date datetime,
-  content varchar(200),
-  is_deleted boolean
-)
+DROP TABLE IF EXISTS `msg_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `msg_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(30) DEFAULT NULL,
+  `article_key` varchar(100) DEFAULT NULL,
+  `post_date` datetime DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `is_deleted` tinyint(1) DEFAULT '0',
+  `content` varchar(200) DEFAULT NULL,
+  `upvote` int(11) DEFAULT '0',
+  `downvote` int(11) DEFAULT '0',
+  `user_agent` varchar(150) DEFAULT NULL,
+  `ref_id` int(11) DEFAULT '-1',
+  `website` varchar(100) DEFAULT NULL,
+  `root_id` int(11) DEFAULT '-1',
+  PRIMARY KEY (`id`),
+  KEY `msg_comment_article_key_index` (`article_key`),
+  KEY `msg_comment_ref_id_index` (`ref_id`),
+  KEY `msg_comment_root_id_index` (`root_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-CREATE INDEX msg_comment_article_key_index ON msg_comment (article_key);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-17 17:48:45
